bf2adc0da763c86f027a612aac441ff2e4581ef3
mcekov <mtsekov@zohomail.eu>
2024-12-23 07:12:04 +0200

    Fix in db seed.js

--- database/seed.js
+++ database/seed.js
@@ -5,4 +5,16 @@ import { createClient } from '@supabase/supabase-js';
 
 const supabase = createClient(process.env.VITE_SUPABASE_URL, process.env.SERVICE_ROLE_KEY);
 
-console.log('Seeding database...', supabase);
+const seedProjects = async () => {
+  const name = faker.lorem.words(2);
+  console.log('Seeding projects...');
+
+  await supabase.from('projects').insert({
+    name: faker.lorem.words(3),
+    slug: name.toLowerCase().replace(/ /g, '-'),
+    status: faker.helpers.arrayElement(['in-progress', 'completed']),
+    collaborators: faker.helpers.arrayElements([1, 2, 3]),
+  });
+};
+
+await seedProjects();
