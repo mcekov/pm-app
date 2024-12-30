1cd2ef7572039fb12a01e07a682277c3121b294d
mcekov <mtsekov@zohomail.eu>
2024-12-30 11:18:57 +0200

    Add tasks_schema

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,5 @@
 <script setup lang="ts">
-import { supabase } from '@/lib/supabaseClient';
-
-console.log(supabase);
+// import { supabase } from '@/lib/supabaseClient';
 </script>
 
 <template>
--- src/pages/projects/index.vue
+++ src/pages/projects/index.vue
@@ -1,8 +1,22 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { supabase } from '@/lib/supabaseClient';
+import { ref } from 'vue';
+import type { Tables } from '../../../database/types';
+
+const projects = ref<Tables<'projects'>[] | null>(null);
+
+(async () => {
+  const { data, error } = await supabase.from('projects').select('*');
+  if (error) console.error(error);
+
+  projects.value = data;
+})();
+</script>
 
 <template>
   <div>Project Page</div>
-  <router-link to="/">Go to home</router-link>
+  <RouterLink to="/">Go to home</RouterLink>
+  {{ projects }}
 </template>
 
-<style lang=""></style>
+<style></style>
