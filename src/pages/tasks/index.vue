c05c009f298b087e1fedf3e7d00d04a0d9602aaa
mcekov <mtsekov@zohomail.eu>
2025-01-03 22:04:08 +0200

    Add Data Table

--- src/pages/tasks/index.vue
+++ src/pages/tasks/index.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 import { supabase } from '@/lib/supabaseClient';
-import { ref } from 'vue';
+import { h, ref } from 'vue';
 import type { Tables } from '../../../database/types';
 import Button from '@/components/ui/button/Button.vue';
+import type { ColumnDef } from '@tanstack/vue-table';
+import DataTable from '@/components/ui/data-table/DataTable.vue';
 
 const tasks = ref<Tables<'tasks'>[] | null>(null);
 
@@ -12,16 +14,49 @@ const tasks = ref<Tables<'tasks'>[] | null>(null);
 
   tasks.value = data;
 })();
+
+/* TABLE */
+interface Payment {
+  id: string;
+  amount: number;
+  status: 'pending' | 'processing' | 'success' | 'failed';
+  email: string;
+}
+
+const payments: Payment[] = [
+  {
+    id: '728ed52f',
+    amount: 100,
+    status: 'pending',
+    email: 'm@example.com',
+  },
+  {
+    id: '489e1d42',
+    amount: 125,
+    status: 'processing',
+    email: 'example@gmail.com',
+  },
+];
+
+const columns: ColumnDef<Payment>[] = [
+  {
+    accessorKey: 'amount',
+    header: () => h('div', { class: 'text-right' }, 'Amount'),
+    cell: ({ row }) => {
+      const amount = Number.parseFloat(row.getValue('amount'));
+      const formatted = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+      }).format(amount);
+
+      return h('div', { class: 'text-right font-medium' }, formatted);
+    },
+  },
+];
 </script>
 
 <template>
-  <div>Tasks Page</div>
-  <RouterLink to="/">Go to home</RouterLink>
-  <ul>
-    <li v-for="task in tasks" key="task.id">{{ task.name }}</li>
-  </ul>
-
-  <Button>Button</Button>
+  <DataTable :columns="columns" :data="payments" />
 </template>
 
 <style></style>
