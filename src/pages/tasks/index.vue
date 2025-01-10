32c1e4a9a2691c5395b720bb46d5684453ca0d44
mcekov <mtsekov@zohomail.eu>
2025-01-10 09:58:52 +0200

    Handle Complex Supabase Queries

--- src/pages/tasks/index.vue
+++ src/pages/tasks/index.vue
@@ -8,17 +8,23 @@ import DataTable from '@/components/ui/data-table/DataTable.vue';
 import { RouterLink } from 'vue-router';
 
 import { usePageStore } from '@/stores/page';
+import type { QueryData } from '@supabase/supabase-js';
 
 usePageStore().pageData.title = 'Tasks';
 
-const tasks = ref<Tables<'tasks'>[] | null>(null);
-
-const getTasks = async () => {
-  const { data, error } = await supabase.from('tasks').select(
-    `
+const tasksWithProjectsQuery = supabase.from('tasks').select(
+  `
     *, projects(id, name, slug)
     `,
-  );
+);
+
+type TasksWithProjects = QueryData<typeof tasksWithProjectsQuery>;
+
+const tasks = ref<TasksWithProjects | null>(null);
+
+const getTasks = async () => {
+  const { data, error } = await tasksWithProjectsQuery;
+
   if (error) console.error(error);
 
   tasks.value = data;
@@ -26,11 +32,9 @@ const getTasks = async () => {
 
 await getTasks();
 
-// console.log('tasks', tasks.value);
-
 /* TABLE */
 
-const columns: ColumnDef<Tables<'tasks'>>[] = [
+const columns: ColumnDef<TasksWithProjects[0]>[] = [
   {
     accessorKey: 'name',
     header: () => h('div', { class: 'text-left' }, 'Name'),
@@ -63,14 +67,16 @@ const columns: ColumnDef<Tables<'tasks'>>[] = [
     accessorKey: 'projects',
     header: () => h('div', { class: 'text-left' }, 'Project'),
     cell: ({ row }) => {
-      return h(
-        RouterLink,
-        {
-          to: `/projects/${row.original.projects.slug}`,
-          class: 'text-left font-medium hover:bg-muted block w-full',
-        },
-        () => row.getValue('projects').name,
-      );
+      return row.original.projects
+        ? h(
+            RouterLink,
+            {
+              to: `/projects/${row.original.projects.slug}`,
+              class: 'text-left font-medium hover:bg-muted block w-full',
+            },
+            () => row.original.projects?.name,
+          )
+        : '';
     },
   },
   {
