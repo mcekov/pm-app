01010778bcb33dc2b1e38e37cc51ca5ea9152c26
Momchil Tsekov <mtsekov@zohomail.eu>
2025-03-02 14:45:34 +0200

    Create a Toggle Component for the Project Status

---
+++ src/components/AppInPlaceEdit/AppInPlaceEditStatus.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const value = defineModel<'in-progress' | 'completed'>();
+</script>
+
+<template>
+  <div class="text-2xl cursor-pointer">
+    <iconify-icon
+      v-if="value === 'completed'"
+      icon="lucide:circle-check"
+      class="text-green-500"
+    ></iconify-icon>
+    <iconify-icon v-else icon="lucide:circle-dot" class="text-gray-50"></iconify-icon>
+  </div>
+</template>
+
+<style scoped></style>
--- src/pages/projects/[slug].vue
+++ src/pages/projects/[slug].vue
@@ -27,12 +27,12 @@ await getProject(slug);
     <TableRow>
       <TableHead>Description</TableHead>
       <TableCell>
-        {{ project.description }}
+        <AppInPlaceEditText v-model="project.description" @commit="updateProject" />
       </TableCell>
     </TableRow>
     <TableRow>
       <TableHead> Status </TableHead>
-      <TableCell>In progress</TableCell>
+      <TableCell><AppInPlaceEditStatus v-model="project.status" /></TableCell>
     </TableRow>
     <TableRow>
       <TableHead> Collaborators </TableHead>
