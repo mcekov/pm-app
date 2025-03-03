440a89af78b58d73da624e3d690cd39e880fac89
Momchil Tsekov <mtsekov@zohomail.eu>
2025-03-03 09:18:27 +0200

    Fix transitions

--- src/App.vue
+++ src/App.vue
@@ -25,19 +25,22 @@ const GuestLayout = defineAsyncComponent(() => import('./components/Layout/main/
   <Transition name="fade" mode="out-in">
     <Component :is="user ? AuthLayout : GuestLayout" :key="user?.id">
       <AppErrorPage v-if="errorStore.activeError" />
+
       <RouterView v-else v-slot="{ Component, route }">
-        <div class="w-full" :key="route.path">
-          <Suspense timeout="0">
-            <Component v-if="Component" :is="Component" />
-            <template #fallback>
-              <div
-                class="absolute top-1/2 transform -translate-y-1/2 left-1/2 -translate-x-1/2 flex justify-center items-center w-full h-screen bg-background bg-opacity-90 z-50"
-              >
-                <iconify-icon class="animate-spin text-6xl" icon="lucide:loader-circle" />
-              </div>
-            </template>
-          </Suspense>
-        </div>
+        <Transition name="fade" mode="out-in">
+          <div class="w-full" :key="route.path">
+            <Suspense timeout="0">
+              <Component v-if="Component" :is="Component" />
+              <template #fallback>
+                <div
+                  class="absolute top-1/2 transform -translate-y-1/2 left-1/2 -translate-x-1/2 flex justify-center items-center w-full h-screen bg-background bg-opacity-90 z-50"
+                >
+                  <iconify-icon class="animate-spin text-6xl" icon="lucide:loader-circle" />
+                </div>
+              </template>
+            </Suspense>
+          </div>
+        </Transition>
       </RouterView>
     </Component>
   </Transition>
