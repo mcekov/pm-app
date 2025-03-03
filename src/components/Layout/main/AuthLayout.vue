b250c5ec18865feddf3dcf1331c221b37b44fed8
Momchil Tsekov <mtsekov@zohomail.eu>
2025-03-03 09:15:11 +0200

    Combine RouterView and Suspense with the Vue Transition Component

--- src/components/Layout/main/AuthLayout.vue
+++ src/components/Layout/main/AuthLayout.vue
@@ -13,16 +13,21 @@ const { menuOpen } = useMenu();
 </script>
 
 <template>
-  <Sidebar @task-cliked="taskSheetOpen = true" />
-  <AppNewTask v-model="taskSheetOpen" />
+  <div>
+    <Sidebar @task-cliked="taskSheetOpen = true" />
+    <AppNewTask v-model="taskSheetOpen" />
 
-  <div class="flex flex-col transition-[margin]" :class="{ 'ml-52': menuOpen, 'ml-24': !menuOpen }">
-    <TopNavbar />
-    <main class="flex flex-col flex-1 gap-4 p-4 lg:gap-4 lg:p-6">
-      <div class="flex items-center">
-        <h1 class="text-lg font-semibold md:text-2xl">{{ pageData.title }}</h1>
-      </div>
-      <slot />
-    </main>
+    <div
+      class="flex flex-col transition-[margin]"
+      :class="{ 'ml-52': menuOpen, 'ml-24': !menuOpen }"
+    >
+      <TopNavbar />
+      <main class="flex flex-col flex-1 gap-4 p-4 lg:gap-4 lg:p-6">
+        <div class="flex items-center">
+          <h1 class="text-lg font-semibold md:text-2xl">{{ pageData.title }}</h1>
+        </div>
+        <slot />
+      </main>
+    </div>
   </div>
 </template>
